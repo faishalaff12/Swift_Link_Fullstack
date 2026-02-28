b2041aac09eb500123a76bda11060a189fe77b49
Muhammad Faishal Affianto <affianto.faishal@gmail.com>
2026-03-01 00:33:32 +0700

    update

--- main.py
+++ main.py
@@ -65,10 +65,3 @@ def read_index():
     # Membaca file HTML Frontend
     with open("index.html", "r", encoding="utf-8") as f:
         return f.read()
-
-if __name__ == "__main__":
-    import os
-    import uvicorn
-    # Mengambil port otomatis dari Railway, atau pakai 8000 kalau di laptop lokal
-    port = int(os.environ.get("PORT", 8000))
-    uvicorn.run("main:app", host="0.0.0.0", port=port)
